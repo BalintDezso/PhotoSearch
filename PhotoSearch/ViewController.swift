868595d4621ce8e6da9a380ec41bf46e22565e72
Balint Dezso <balint2005@gmail.com>
2018-09-16 11:12:15 +0300

    Empty project.

---
+++ PhotoSearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoSearch
+//
+//  Created by Diana Ivascu on 9/16/18.
+//  Copyright © 2018 Balint Dezso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
