f4a011dd056be6a027fae3a7c041da3fde7c0049
Balint Dezso <balint2005@gmail.com>
2018-09-16 11:15:56 +0300

    Created project structure.

---
+++ Service/Service.h
@@ -0,0 +1,19 @@
+//
+//  Service.h
+//  Service
+//
+//  Created by Diana Ivascu on 9/16/18.
+//  Copyright © 2018 Balint Dezso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Service.
+FOUNDATION_EXPORT double ServiceVersionNumber;
+
+//! Project version string for Service.
+FOUNDATION_EXPORT const unsigned char ServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Service/PublicHeader.h>
+
+
